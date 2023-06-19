8808ca3cd33a4927f4d5cb0116c41d1c3de48611
Chinmay Chamoli <128532140+CapstoneGroup1StClair@users.noreply.github.com>
2023-06-19 07:22:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def get_form_submission():
     data = request.get_json()
     admin_emails = [user['email'] for user in db.Users.find()]
     total_score = process_answer(data)
-    if total_score > 20:
+    if total_score > 102:
         severity = 'RED'
-    elif total_score > 12:
+    elif total_score > 51:
         severity = 'AMBER'
     data['severity'] = severity
     data['score'] = total_score
